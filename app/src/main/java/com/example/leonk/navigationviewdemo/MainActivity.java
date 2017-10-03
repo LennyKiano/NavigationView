2d7104afcf3067c58140242428dbc01afd6e7bb4
LennyKiano <leonkiano@gmail.com>
2017-10-03 15:46:15 +0300

    Adding a tool bar and drawer Toggle(hamburger icon)

--- app/src/main/java/com/example/leonk/navigationviewdemo/MainActivity.java
+++ app/src/main/java/com/example/leonk/navigationviewdemo/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.leonk.navigationviewdemo;
 
+import android.content.res.Configuration;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private NavigationView mDrawer;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar=(Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        mDrawer=(NavigationView) findViewById(R.id.main_drawer);
+        mDrawerLayout=(DrawerLayout) findViewById(R.id.drawer_layout);
+
+        //for hamburger icon
+        drawerToggle= new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close);
+
+
+        mDrawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+    }
+
+    //needs to be called for the drawer toggle
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 }
